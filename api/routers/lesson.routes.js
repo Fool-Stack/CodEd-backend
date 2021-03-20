c31114153bee186265e704f6e91e168b03ebc1fa
Shivam Mehta <shivam.mehta2019@vitstudent.ac.in>
2021-03-20 15:27:41 +0530

    Procfilr

--- api/routers/lesson.routes.js
+++ api/routers/lesson.routes.js
@@ -4,17 +4,18 @@ const lessonController = require("../controllers/lesson.controller");
 const { upload } = require("../middlewares/s3UploadClient");
 const checkAuth = require("../middlewares/checkAuth");
 const checkAuthAdmin = require("../middlewares/checkAuthAdmin");
-
+const code = require('../controllers/code')
 const router = express.Router();
 
 router.post("/add", checkAuthAdmin, upload.single('video'), lessonController.addLesson);
 
-router.post("/addLessonWithoutVideo", checkAuthAdmin, lessonController.addLessonWithoutVideo);
+router.post("/withoutVideo", checkAuthAdmin, lessonController.addLessonWithoutVideo);
 
 router.patch("/update", checkAuthAdmin, lessonController.updateLesson);
 
-router.post("/get", checkAuth, lessonController.getLesson);
+router.get("/get", checkAuth, lessonController.getLesson);
 
+router.get("/all",checkAuth,lessonController.getAllLessons)
 router.post(
   "/checkEnrolled",
   checkAuth,
@@ -23,5 +24,10 @@ router.post(
 
 router.delete("/deleteLesson", checkAuthAdmin, lessonController.deleteLesson)
 
+router.post("/code",checkAuth,code.submit )
+
+router.post('/token',checkAuth,code.getToken)
+
+router.post('/output',checkAuth,code.getOuput)
 
 module.exports = router;
